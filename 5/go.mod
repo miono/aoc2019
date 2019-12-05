84025c6c631ebf5e6edb19bdf09a5c51803e5cf2
Markus Nilsson <markus.nilsson@yubico.com>
2019-12-05 07:59:24 +0100

    Add solution for Day 5b

---
+++ 5/go.mod
@@ -0,0 +1,5 @@
+module five
+
+require github.com/miono/aoc2019/intcode v0.0.0-20191205060726-44ccd44a9c1c
+
+replace github.com/miono/aoc2019/intcode v0.0.0-20191205060726-44ccd44a9c1c => /home/miono/git/aoc2019/intcode
--- intcode/intcode.go
+++ intcode/intcode.go
@@ -22,6 +22,12 @@ func New(state []int, pos int) *Machine {
 	}
 }
 
+func (m *Machine) Run() {
+	for {
+		m.Step()
+	}
+}
+
 func (m *Machine) Step() {
 	instr := m.State[m.Position]
 	switch instr % 100 {
@@ -33,6 +39,14 @@ func (m *Machine) Step() {
 		m.Input()
 	case 4:
 		m.Output()
+	case 5:
+		m.JumpIfTrue()
+	case 6:
+		m.JumpIfFalse()
+	case 7:
+		m.LessThan()
+	case 8:
+		m.Equals()
 	case 99:
 		m.Halt()
 	}
@@ -63,7 +77,7 @@ func (m *Machine) Mult() {
 	param1 := 0
 	param2 := 0
 	instr := m.State[m.Position]
-	instr = instr - 1
+	instr = instr - 2
 	if instr%1000 == 100 {
 		param1 = m.State[m.Position+1]
 		instr = instr - 100
@@ -82,25 +96,128 @@ func (m *Machine) Mult() {
 func (m *Machine) Input() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Give input: ")
-	value, err := reader.ReadString('\n')
+	value, _, err := reader.ReadLine()
 	if err != nil {
 		panic(err)
 	}
-	intValue, err := strconv.Atoi(value)
+	intValue, err := strconv.Atoi(string(value))
 	if err != nil {
 		panic(err)
 	}
 	m.State[m.State[m.Position+1]] = intValue
+	m.Position = m.Position + 2
 
 }
 
 func (m *Machine) Output() {
-	outputVal := m.State[m.State[m.Position+1]]
-	fmt.Println(outputVal)
+	param := 0
+	instr := m.State[m.Position]
+	instr = instr - 4
+	if instr%1000 == 100 {
+		param = m.State[m.Position+1]
+	} else {
+		param = m.State[m.State[m.Position+1]]
+	}
+	fmt.Println(param)
 	m.Position = m.Position + 2
 }
 
-func (m *Machine) Halt() {
-	fmt.Println(m.State)
+func (m *Machine) JumpIfTrue() {
+	param1 := 0
+	param2 := 0
+	instr := m.State[m.Position]
+	instr = instr - 5
+	if instr%1000 == 100 {
+		param1 = m.State[m.Position+1]
+		instr = instr - 100
+	} else {
+		param1 = m.State[m.State[m.Position+1]]
+	}
+	if instr%10000 == 1000 {
+		param2 = m.State[m.Position+2]
+	} else {
+		param2 = m.State[m.State[m.Position+2]]
+	}
+	if param1 != 0 {
+		m.Position = param2
+	} else {
+		m.Position = m.Position + 3
+	}
+}
 
+func (m *Machine) JumpIfFalse() {
+	param1 := 0
+	param2 := 0
+	instr := m.State[m.Position]
+	instr = instr - 6
+	if instr%1000 == 100 {
+		param1 = m.State[m.Position+1]
+		instr = instr - 100
+	} else {
+		param1 = m.State[m.State[m.Position+1]]
+	}
+	if instr%10000 == 1000 {
+		param2 = m.State[m.Position+2]
+	} else {
+		param2 = m.State[m.State[m.Position+2]]
+	}
+	if param1 == 0 {
+		m.Position = param2
+	} else {
+		m.Position = m.Position + 3
+	}
+}
+
+func (m *Machine) LessThan() {
+	param1 := 0
+	param2 := 0
+	instr := m.State[m.Position]
+	instr = instr - 7
+	if instr%1000 == 100 {
+		param1 = m.State[m.Position+1]
+		instr = instr - 100
+	} else {
+		param1 = m.State[m.State[m.Position+1]]
+	}
+	if instr%10000 == 1000 {
+		param2 = m.State[m.Position+2]
+	} else {
+		param2 = m.State[m.State[m.Position+2]]
+	}
+	if param1 < param2 {
+		m.State[m.State[m.Position+3]] = 1
+		m.Position = m.Position + 4
+	} else {
+		m.State[m.State[m.Position+3]] = 0
+		m.Position = m.Position + 4
+	}
+}
+
+func (m *Machine) Equals() {
+	param1 := 0
+	param2 := 0
+	instr := m.State[m.Position]
+	instr = instr - 8
+	if instr%1000 == 100 {
+		param1 = m.State[m.Position+1]
+		instr = instr - 100
+	} else {
+		param1 = m.State[m.State[m.Position+1]]
+	}
+	if instr%10000 == 1000 {
+		param2 = m.State[m.Position+2]
+	} else {
+		param2 = m.State[m.State[m.Position+2]]
+	}
+	if param1 == param2 {
+		m.State[m.State[m.Position+3]] = 1
+		m.Position = m.Position + 4
+	} else {
+		m.State[m.State[m.Position+3]] = 0
+		m.Position = m.Position + 4
+	}
+}
+
+func (m *Machine) Halt() {
+	os.Exit(0)
 }
